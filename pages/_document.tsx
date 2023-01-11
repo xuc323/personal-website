21d074f3316bd35beee2c7e36b1ff6e36127d83a
xuc323 <79107215+xuc323@users.noreply.github.com>
2023-01-10 22:37:52 -0500

    Added google tags

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,10 +1,24 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
       <body className="relative pb-16 m-0 min-h-screen bg-gray-50 dark:bg-stone-900 duration-300">
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-WDE3KLWSEF"
+          strategy="beforeInteractive"
+        />
+        <Script id="google-tag" strategy="beforeInteractive">
+          {`window.dataLayer = window.dataLayer || [];
+            function gtag(){
+              dataLayer.push(arguments);
+            }
+            gtag('js', new Date());
+            gtag('config', 'G-WDE3KLWSEF');
+          `}
+        </Script>
         <Main />
         <NextScript />
       </body>
