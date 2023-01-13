b1575824725e8ef4b69a9e2c192b2859f6c58203
Xu Chen <79107215+xuc323@users.noreply.github.com>
2023-01-12 22:46:54 -0500

    Altered URL for project and filled computer graphics page (#5)

--- pages/project/index.tsx
+++ pages/project/index.tsx
@@ -9,26 +9,28 @@ export default function Project() {
         <h1 className="text-center text-4xl text-black font-bold dark:text-white">
           Project Experiences
         </h1>
-        <div className="w-10/12 mx-auto mt-8 rounded-2xl shadow-lg bg-white max-w-3xl dark:bg-black max-sm:w-full">
+        <div className="w-10/12 mx-auto mt-8 rounded-2xl max-w-3xl shadow-lg bg-white duration-300 max-sm:w-full max-sm:shadow-none dark:bg-black">
           <ul className="grid grid-cols-3 p-5 text-blue-500 max-md:grid-cols-2 max-sm:grid-cols-1">
             <li className="m-2 border p-2 text-center rounded-lg min-h-[200px]">
-              <Link href="/project/welcomebot">Discord Welcome Bot</Link>
+              <Link href="/project/welcome-bot">Discord Welcome Bot</Link>
               <p className="text-black dark:text-white"></p>
             </li>
             <li className="m-2 border p-2 text-center rounded-lg min-h-[200px]">
-              <Link href="/project/personalweb">Personal Website</Link>
+              <Link href="/project/personal-website">Personal Website</Link>
             </li>
             <li className="m-2 border p-2 text-center rounded-lg min-h-[200px]">
-              <Link href="/project/visualization">STEM Visualization</Link>
+              <Link href="/project/stem-visualization">STEM Visualization</Link>
             </li>
             <li className="m-2 border p-2 text-center rounded-lg min-h-[200px]">
-              <Link href="/project/graphics">Computer Graphics</Link>
+              <Link href="/project/computer-graphics">Computer Graphics</Link>
             </li>
             <li className="m-2 border p-2 text-center rounded-lg min-h-[200px]">
-              <Link href="/project/quizbowl">Music Quiz Bowl</Link>
+              <Link href="/project/music-quiz-bowl">Music Quiz Bowl</Link>
             </li>
             <li className="m-2 border p-2 text-center rounded-lg min-h-[200px]">
-              <Link href="/project/grader">CS Teaching Assistant / Grader</Link>
+              <Link href="/project/lehigh-grader">
+                CS Teaching Assistant / Grader
+              </Link>
             </li>
           </ul>
         </div>
