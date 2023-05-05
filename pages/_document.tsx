229e9ff0b1f52702df2063bd89d97b2dad691434
xuc323 <79107215+xuc323@users.noreply.github.com>
2023-05-05 12:17:38 -0400

    G-tag is now from environment

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -7,16 +7,15 @@ export default function Document() {
       <Head>
         {/* Google tag (gtag.js) */}
         <Script
-          src="https://www.googletagmanager.com/gtag/js?id=G-WDE3KLWSEF"
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_ANALYTICS_TAG}`}
           strategy="afterInteractive"
         />
         <Script id="google-analytics" strategy="afterInteractive">
           {`
             window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
+            function gtag() { dataLayer.push(arguments); }
             gtag('js', new Date());
-
-            gtag('config', 'G-WDE3KLWSEF');
+            gtag('config', '${process.env.NEXT_PUBLIC_ANALYTICS_TAG}');
           `}
         </Script>
       </Head>
