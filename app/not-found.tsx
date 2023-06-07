e6bd9ff9cd0b7f0320e3cd70ca8f19b1bfe5ba5b
xuc323 <79107215+xuc323@users.noreply.github.com>
2023-06-07 10:23:29 -0400

    render 404 in app dir

---
+++ app/not-found.tsx
@@ -0,0 +1,11 @@
+export default function NotFound() {
+  return (
+    <main>
+      <div className="flex h-72 justify-center items-center">
+        <h1 className="font-bold text-5xl text-center text-black dark:text-white duration-300">
+          404 &middot; Page Not Found
+        </h1>
+      </div>
+    </main>
+  );
+}
