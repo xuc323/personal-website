bf0b47fb2d9392a6278c342d3527a809808bc791
xuc323 <79107215+xuc323@users.noreply.github.com>
2023-08-12 14:10:40 -0400

    Prepare for cloud function

---
+++ app/not-found.tsx
@@ -0,0 +1,9 @@
+export default function NotFound() {
+  return (
+    <div className="flex flex-col h-72 justify-center items-center">
+      <h1 className="m-5 p-5 font-bold text-5xl">
+        404 &middot; Page Not Found
+      </h1>
+    </div>
+  );
+}
