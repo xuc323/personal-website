2178157c979101d97e173d522eca34f71dfc4114
Xu Chen <79107215+xuc323@users.noreply.github.com>
2023-09-11 10:44:01 -0400

    Added image api

--- app/hello/route.ts
+++ app/hello/route.ts
@@ -1,5 +1,5 @@
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
-export async function GET(req: Request) {
+export async function GET(req: NextRequest) {
   return NextResponse.json("Hello");
 }
