6b29ea368048549848a120df6637abdcdac0d84a
Xu Chen <79107215+xuc323@users.noreply.github.com>
2023-09-11 17:14:49 -0400

    Specify runtime in routes

--- app/images/route.ts
+++ app/images/route.ts
@@ -1,5 +1,9 @@
 import { NextRequest, NextResponse } from "next/server";
 
+export const dynamic = "force-dynamic";
+
+export const runtime = "edge";
+
 export async function GET(req: NextRequest) {
   const num = Math.floor(Math.random() * 10 + 1);
 
