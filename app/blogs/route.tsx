7375c86f198dabdd2b42fe20cdf874ec79549043
Xu Chen <79107215+xuc323@users.noreply.github.com>
2024-04-22 00:09:56 -0400

    Export edge runtime

--- app/blogs/route.tsx
+++ app/blogs/route.tsx
@@ -2,6 +2,8 @@ import { NextRequest, NextResponse } from "next/server";
 
 export const dynamic = "force-dynamic";
 
+export const runtime = "edge";
+
 export async function GET(request: NextRequest) {
   const num = Math.random() * 100;
 
