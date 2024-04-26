7b07c714d6f73412543a796fb366a82e66ef497b
Xu Chen <79107215+xuc323@users.noreply.github.com>
2024-04-25 20:44:14 -0400

    added edge for necessary files

--- app/blogs/route.tsx
+++ app/blogs/route.tsx
@@ -1,7 +1,5 @@
 import { NextRequest, NextResponse } from "next/server";
 
-export const dynamic = "force-dynamic";
-
 export const runtime = "edge";
 
 export async function GET(request: NextRequest) {
