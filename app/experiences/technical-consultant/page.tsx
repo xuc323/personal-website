ab6b56aa5f2224c70042d663943ea5fe3bbbfd07
Xu Chen <79107215+xuc323@users.noreply.github.com>
2024-10-02 16:43:59 -0400

    Prefix links with constant

--- app/experiences/technical-consultant/page.tsx
+++ app/experiences/technical-consultant/page.tsx
@@ -1,3 +1,4 @@
+import { PageContainer } from "@/components/ContainerUtils";
 import { TitleContainer } from "@/components/TextUtils";
 import type { Metadata } from "next";
 
@@ -7,6 +8,10 @@ export default function Experience() {
   return (
     <div className="py-3">
       <TitleContainer>Technical Consultant</TitleContainer>
+
+      <PageContainer>
+        <div></div>
+      </PageContainer>
     </div>
   );
 }
