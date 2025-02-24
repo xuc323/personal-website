984d752e76f52858a602ca1679b67c29a9ab5e6e
Xu Chen <79107215+xuc323@users.noreply.github.com>
2025-02-24 17:19:14 -0500

    Moved to src dir

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,7 +5,9 @@ import { fileURLToPath } from "url";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-const compat = new FlatCompat({ baseDirectory: __dirname });
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,3 +1,4 @@
+import { BASE_DOMAIN } from "@/constants";
 import Link from "next/link";
 import { BsGithub } from "react-icons/bs";
 import { IoMdMail } from "react-icons/io";
@@ -8,13 +9,15 @@ export default function Footer() {
   const year = date.getFullYear();
 
   return (
-    <footer className="absolute bottom-0 w-full border-t-2 bg-gray-100 pb-4 pt-3 text-center font-serif text-black dark:bg-black dark:text-white max-sm:px-2">
+    <footer className="absolute bottom-0 w-full border-t-2 bg-gray-100 pb-4 pt-3 text-center font-serif text-black max-sm:px-2 dark:bg-black dark:text-white">
       <div className="mx-auto flex max-w-4xl items-center justify-between px-2 text-center">
         <div className="flex w-1/12 justify-start">
           <DarkModeIcon />
         </div>
         <div className="grow items-center justify-center">
-          <p>Copyright &copy; {year}, www.xchen.org</p>
+          <p>
+            Copyright &copy; {year}, {BASE_DOMAIN}
+          </p>
         </div>
         <div className="flex w-1/12 justify-end gap-3 max-sm:w-2/12">
           <Link
