c94344b62737dd0c20c5a65b14aac3a950e006ea
Xu Chen <79107215+xuc323@users.noreply.github.com>
2025-07-05 10:25:29 -0400

    Added wrangler

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,14 @@
 import mdx from "@next/mdx";
+import { initOpenNextCloudflareForDev } from "@opennextjs/cloudflare";
 import type { NextConfig } from "next";
 
 const withMDX = mdx();
 
 const nextConfig: NextConfig = {
-  output: "standalone",
   pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
 };
+
+// added by create cloudflare to enable calling `getCloudflareContext()` in `next dev`
+initOpenNextCloudflareForDev();
 
 export default withMDX(nextConfig);
