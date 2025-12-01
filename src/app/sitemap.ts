9363dec6ac9a989895086a214d66008ef6c88ceb
Xu Chen <79107215+xuc323@users.noreply.github.com>
2025-11-30 20:50:29 -0500

    sitemap now using build time

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,9 +1,9 @@
 import { BASE_URL } from "@/utils/constants";
 import type { MetadataRoute } from "next";
-import { execSync } from "node:child_process";
 import { readdirSync } from "node:fs";
 
 const FILE_PATTERN = /page\.(ts|tsx|md|mdx)$/;
+const BUILD_TIME = new Date();
 
 export default async function Sitemap(): Promise<MetadataRoute.Sitemap> {
   const files = readdirSync("./src/app", {
@@ -14,22 +14,10 @@ export default async function Sitemap(): Promise<MetadataRoute.Sitemap> {
   return files.map((file) => {
     return {
       url: getUrl(file, BASE_URL),
-      lastModified: getLastModified(file),
+      lastModified: BUILD_TIME,
       changeFrequency: "monthly",
     };
   });
-}
-
-function getLastModified(filePath: string): Date {
-  try {
-    const timestamp = execSync(
-      `git log -1 --format=%aI -- src/app/${filePath}`,
-      { encoding: "utf-8" }
-    ).trim();
-    return new Date(timestamp);
-  } catch {
-    return new Date();
-  }
 }
 
 function getUrl(file: string, base: string) {
